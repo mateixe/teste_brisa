ff3767d77785358a39ef8b12690a0bfa87f1ccc3
mateixe <mateixe@gmail.com>
2025-02-13 02:07:24 -0300

    correcting the bloc injection

--- lib/presentation/pages/product_list/views/product_list_view.dart
+++ lib/presentation/pages/product_list/views/product_list_view.dart
@@ -4,59 +4,33 @@ import 'package:teste_brisa/model/data_objects/product.dart';
 import 'package:teste_brisa/model/repository/product_repository.dart';
 import 'package:teste_brisa/presentation/pages/product_list/bloc/product_list_bloc.dart';
 
-class ProductListView extends StatefulWidget {
+class ProductListView extends StatelessWidget {
   final ProductListBloc _bloc = ProductListBloc(ProductsRepository());
 
-  ProductListView({super.key});
-
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //     body: BlocBuilder<ProductListBloc, ProductListDataState>(
-  //         bloc: _bloc,
-  //         builder: (context, state) {
-  //           return state is! ProductListDataLoadedState
-  //               ? const SafeArea(child: Placeholder())
-  //               : SafeArea(
-  //                   child: ListView.builder(
-  //                     itemBuilder: (context, index) {
-  //                       return ListTile(
-  //                         tileColor: Colors.white60,
-  //                         title: Text(state.apiResult[index].title),
-  //                         subtitle: Text(state.apiResult[index].body),
-  //                       );
-  //                     },
-  //                     itemCount: state.apiResult.length,
-  //                   ),
-  //                 );
-  //         }),
-  //   );
-  // }
-
-  @override
-  State<StatefulWidget> createState() => _ProductListViewState(_bloc);
-}
-
-class _ProductListViewState extends State<ProductListView> {
-  ProductListBloc _bloc;
-
-  _ProductListViewState(this._bloc);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _bloc.close();
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    // _bloc = ProductListBloc(ProductsRepository());
+  ProductListView({super.key}) {
     _bloc.add(LoadProductDataEvent());
+  }
 
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BlocBuilder<ProductListBloc, ProductListDataState>(
+          bloc: _bloc,
+          builder: (context, state) {
+            return state is! ProductListDataLoadedState
+                ? const SafeArea(child: Placeholder())
+                : SafeArea(
+                    child: ListView.builder(
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          tileColor: Colors.white60,
+                          title: Text(state.apiResult[index].title),
+                          subtitle: Text(state.apiResult[index].body),
+                        );
+                      },
+                      itemCount: state.apiResult.length,
+                    ),
+                  );
+          }),
+    );
   }
 }
